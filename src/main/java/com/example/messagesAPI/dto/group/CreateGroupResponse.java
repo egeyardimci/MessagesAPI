7d9c2fa561bbcdaec5b39fbe441066c7d78f9989
egeyardimci <ege.yardimci@sabanciuniv.edu>
2024-11-11 17:01:25 +0300

    fixed groupID response

--- src/main/java/com/example/messagesAPI/dto/group/CreateGroupResponse.java
+++ src/main/java/com/example/messagesAPI/dto/group/CreateGroupResponse.java
@@ -1,8 +1,11 @@
 package com.example.messagesAPI.dto.group;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.bson.types.ObjectId;
 
 public record CreateGroupResponse(
+        @JsonSerialize(using = ToStringSerializer.class)
         ObjectId id
 ) {
 }
